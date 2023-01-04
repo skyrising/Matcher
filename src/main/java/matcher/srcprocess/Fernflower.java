81737b653212f93872953d8e7edb77a578b91e32
sschr15 <steven.sschr15@hotmail.com>
2023-01-03 18:43:42 -0600

    Update components (#1)

--- src/main/java/matcher/srcprocess/Fernflower.java
+++ src/main/java/matcher/srcprocess/Fernflower.java
@@ -1,30 +1,15 @@
 package matcher.srcprocess;
 
 import matcher.NameType;
-import matcher.type.ClassEnv;
 import matcher.type.ClassFeatureExtractor;
 import matcher.type.ClassInstance;
-import org.jetbrains.java.decompiler.main.ClassesProcessor;
-import org.jetbrains.java.decompiler.main.ClassesProcessor.ClassNode;
-import org.jetbrains.java.decompiler.main.DecompilerContext;
-import org.jetbrains.java.decompiler.main.IdentityRenamerFactory;
+import org.jetbrains.java.decompiler.main.decompiler.BaseDecompiler;
 import org.jetbrains.java.decompiler.main.decompiler.PrintStreamLogger;
 import org.jetbrains.java.decompiler.main.extern.*;
-import org.jetbrains.java.decompiler.modules.renamer.ConverterHelper;
-import org.jetbrains.java.decompiler.modules.renamer.IdentifierConverter;
-import org.jetbrains.java.decompiler.modules.renamer.PoolInterceptor;
-import org.jetbrains.java.decompiler.struct.ContextUnit;
-import org.jetbrains.java.decompiler.struct.IDecompiledData;
-import org.jetbrains.java.decompiler.struct.StructClass;
-import org.jetbrains.java.decompiler.struct.StructContext;
-import org.jetbrains.java.decompiler.struct.lazy.LazyLoader;
-import org.jetbrains.java.decompiler.util.DataInputFullStream;
-import org.jetbrains.java.decompiler.util.TextBuffer;
 
-import java.io.FileNotFoundException;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.UncheckedIOException;
-import java.lang.reflect.Field;
+import java.io.InputStream;
 import java.util.*;
 import java.util.jar.Manifest;
 
@@ -33,176 +18,83 @@ public class Fernflower implements Decompiler {
 	public String decompile(ClassInstance cls, ClassFeatureExtractor env, NameType nameType) {
 		// invoke ff with on-demand class lookup into matcher's state and string based output
 		Map<String, Object> properties = new HashMap<>(IFernflowerPreferences.DEFAULTS);
-		properties.put(IFernflowerPreferences.DECOMPILE_GENERIC_SIGNATURES, "1");
+		properties.putAll(Map.of(
+				IFernflowerPreferences.REMOVE_SYNTHETIC, "0", // Easier to match synthetic methods
+				IFernflowerPreferences.REMOVE_BRIDGE, "0", // Easier to match bridge methods
+				IFernflowerPreferences.USE_JAD_VARNAMING, "1" // JAD-style names are often more readable
+		));
 
-		ResultSaver resultSaver = new ResultSaver();
-		DecompiledData data = new DecompiledData();
-		BytecodeProvider bcProvider = new BytecodeProvider(env, nameType);
-		MatcherStructContext structContext = new MatcherStructContext(resultSaver, data, new LazyLoader(bcProvider), env, nameType, bcProvider);
-		ClassesProcessor classProcessor = new ClassesProcessor(structContext);
-		PoolInterceptor interceptor = null;
-		IIdentifierRenamer renamer;
-		IdentifierConverter converter;
-
-		if ("1".equals(properties.get(IFernflowerPreferences.RENAME_ENTITIES))) {
-			renamer = new ConverterHelper();
-			interceptor = new PoolInterceptor();
-			converter = new IdentifierConverter(structContext, renamer, interceptor);
-		} else {
-			renamer = null;
-			converter = null;
-		}
-
-		data.classProcessor = classProcessor;
-		data.converter = converter;
-
-		IFernflowerLogger logger = new PrintStreamLogger(System.out);
-		DecompilerContext context = new DecompilerContext(properties, logger, structContext, classProcessor, interceptor, new IdentityRenamerFactory());
-		DecompilerContext.setCurrentContext(context);
-
-		try {
-			structContext.addSpace(cls, true);
-
-			// queue inner classes as well
-			if (!cls.getInnerClasses().isEmpty()) {
-				Queue<ClassInstance> toAdd = new ArrayDeque<>(cls.getInnerClasses());
-				ClassInstance innerCls;
-
-				while ((innerCls = toAdd.poll()) != null) {
-					structContext.addSpace(innerCls, true);
-					toAdd.addAll(innerCls.getInnerClasses());
-				}
-			}
-
-			if (converter != null) converter.rename();
-
-			classProcessor.loadClasses(renamer);
-			structContext.saveContext();
-		} finally {
-			DecompilerContext.setCurrentContext(null);
-		}
-
-		String ret = resultSaver.results.get(cls.getName(nameType));
-
-		if (ret != null) {
-			return ret;
-		} else {
-			throw new RuntimeException("decompiling "+cls+" didn't yield the expected result (available: "+resultSaver.results.keySet()+")");
+		try (ResultSaver resultSaver = new ResultSaver()) {
+			BaseDecompiler decompiler = new BaseDecompiler(
+					resultSaver,
+					properties,
+					new PrintStreamLogger(System.out)
+			);
+			decompiler.addSource(new ClassSource(cls, env, nameType, resultSaver));
+			decompiler.decompileContext();
+			return resultSaver.results.get(cls.getName(nameType));
+		} catch (IOException e) {
+			throw new RuntimeException(e);
 		}
 	}
 
-	private static class MatcherStructContext extends StructContext {
-		@SuppressWarnings("unchecked")
-		MatcherStructContext(IResultSaver saver, IDecompiledData decompiledData, LazyLoader loader, ClassEnv env, NameType nameType, BytecodeProvider bcProvider) {
-			super(saver, decompiledData, loader);
+	private static class ClassSource implements IContextSource {
+		private final ClassInstance cls;
+		private final ClassFeatureExtractor env;
+		private final NameType nameType;
+		private final ResultSaver resultSaver;
 
-			this.loader = loader;
+		public ClassSource(ClassInstance cls, ClassFeatureExtractor env, NameType nameType, ResultSaver resultSaver) {
+			this.cls = cls;
 			this.env = env;
 			this.nameType = nameType;
-			this.bcProvider = bcProvider;
-
-			try {
-				Field f = StructContext.class.getDeclaredField("units");
-				f.setAccessible(true);
-				units = (Map<String, ContextUnit>) f.get(this);
-
-				f = StructContext.class.getDeclaredField("ownClasses");
-				f.setAccessible(true);
-				ownClasses = (Map<String, StructClass>) f.get(this);
-			} catch (ReflectiveOperationException e) {
-				throw new RuntimeException(e);
-			}
-
-			ownedUnit = units.get("");
-			unownedUnit = new ContextUnit(ContextUnit.TYPE_FOLDER, null, unownedUnitFilename, false, saver, decompiledData); // avoids producing superfluous output
-			units.put(unownedUnitFilename, unownedUnit);
-		}
-
-		public void addSpace(ClassInstance cls, boolean isOwn) {
-			try {
-				addStructClass(cls, isOwn);
-			} catch (IOException ex) {
-				String message = "Corrupted class: " + cls.getName(nameType);
-				DecompilerContext.getLogger().writeMessage(message, ex);
-			}
+			this.resultSaver = resultSaver;
 		}
 
 		@Override
-		public StructClass getClass(String name) {
-			if (DEBUG) System.out.printf("getClass(%s)%n", name);
-			if (name == null) return null;
-
-			// use classes as a cache, load anything missing on demand
-			StructClass ret = ownClasses.get(name);
-			if (ret != null) return ret;
-
-			ClassInstance cls = env.getClsByName(name, nameType);
-			if (cls == null) return null;
-
-			try {
-				return addStructClass(cls, false);
-			} catch (IOException e) {
-				throw new UncheckedIOException(e);
-			}
-		}
-
-		private StructClass addStructClass(ClassInstance cls, boolean isOwn) throws IOException {
-			String name = cls.getName(nameType);
-			byte[] data = bcProvider.get(name); // BytecodeProvider has a name->byte[] cache to avoid redundant cls.serialize invocations
-			if (data == null) throw new IllegalStateException();
-
-			StructClass cl = StructClass.create(new DataInputFullStream(data), isOwn, loader);
-			ownClasses.put(cl.qualifiedName, cl);
-
-			ContextUnit unit = isOwn ? ownedUnit : unownedUnit;
-			unit.addClass(cl, name.substring(name.lastIndexOf('/') + 1)+".class");
-			loader.addClassLink(cl.qualifiedName, new LazyLoader.Link(pathPrefix+name+pathSuffix, null));
-
-			return cl;
+		public IOutputSink createOutputSink(IResultSaver saver) {
+			return resultSaver;
 		}
 
 		@Override
-		public Map<String, StructClass> getOwnClasses() {
-			return emulatedClasses;
+		public String getName() {
+			return "Matcher FF Class Source";
 		}
 
-		protected final LazyLoader loader;
-		protected final ClassEnv env;
-		protected final NameType nameType;
-		protected final BytecodeProvider bcProvider;
-		protected final Map<String, ContextUnit> units;
-		protected final Map<String, StructClass> ownClasses;
-		protected final ContextUnit ownedUnit;
-		protected final ContextUnit unownedUnit;
+		private static String getName(ClassInstance cls, NameType nameType) {
+			return Objects.requireNonNullElse(cls.getName(nameType), cls.getName());
+		}
 
-		private final Map<String, StructClass> emulatedClasses = new AbstractMap<String, StructClass>() {
-			@Override
-			public boolean containsKey(Object key) {
-				return get(key) != null;
+		private final Map<String, byte[]> cache = new HashMap<>();
+
+		@Override
+		public Entries getEntries() {
+			List<Entry> entries = new ArrayList<>();
+			String name = getName(cls, nameType);
+			entries.add(Entry.parse(name));
+			cache.put(name, cls.serialize(nameType));
+
+			for (ClassInstance innerCls : cls.getInnerClasses()) {
+				String innerName = getName(innerCls, nameType);
+				entries.add(Entry.parse(innerName));
+				cache.put(innerName, innerCls.serialize(nameType));
 			}
 
-			@Override
-			public StructClass get(Object key) {
-				if (!(key instanceof String)) return null;
+			return new Entries(entries, List.of(), List.of());
+		}
 
-				return MatcherStructContext.this.getClass((String) key);
+		@Override
+		public InputStream getInputStream(String resource) throws IOException {
+			resource = resource.substring(0, resource.length() - ".class".length());
+			if (cache.get(resource) != null) {
+				return new ByteArrayInputStream(cache.get(resource));
+			} else {
+				throw new IOException("Resource not found: "+resource);
 			}
-
-			@Override
-			public int size() {
-				return ownClasses.size();
-			}
-
-			@Override
-			public Set<Map.Entry<String, StructClass>> entrySet() {
-				Set<Map.Entry<String, StructClass>> snapshot = new HashSet<>(ownClasses.entrySet()); // copy to hide concurrent modifications from on-demand additions
-
-				return snapshot;
-			}
-		};
+		}
 	}
 
-	private static class ResultSaver implements IResultSaver {
+	private static class ResultSaver implements IResultSaver, IContextSource.IOutputSink {
 		@Override
 		public void saveFolder(String path) { }
 		@Override
@@ -226,86 +118,28 @@ public class Fernflower implements Decompiler {
 		}
 
 		Map<String, String> results = new HashMap<>();
-	}
 
-	private static class DecompiledData implements IDecompiledData {
 		@Override
-		public String getClassEntryName(StructClass cl, String entryname) {
-			if (DEBUG) System.out.printf("getClassEntryName(%s, %s)%n", cl, entryname);
+		public void begin() { }
 
-			ClassNode node = classProcessor.getMapRootClasses().get(cl.qualifiedName);
+		@Override
+		public void acceptClass(String qualifiedName, String fileName, String content, int[] mapping) {
+			if (DEBUG) System.out.printf("acceptClass(%s, %s, %s, %s)%n", qualifiedName, fileName, content, Arrays.toString(mapping));
 
-			if (node.type != ClassNode.CLASS_ROOT) {
-				return null;
-			} else if (converter != null) {
-				String simpleClassName = cl.qualifiedName.substring(cl.qualifiedName.lastIndexOf('/') + 1);
-				return entryname.substring(0, entryname.lastIndexOf('/') + 1) + simpleClassName + ".java";
-			} else {
-				return entryname.substring(0, entryname.lastIndexOf(".class")) + ".java";
-			}
+			results.put(qualifiedName, content);
 		}
 
 		@Override
-		public String getClassContent(StructClass cl) {
-			if (DEBUG) System.out.printf("getClassContent(%s)%n", cl);
-
-			try {
-				TextBuffer buffer = new TextBuffer(ClassesProcessor.AVERAGE_CLASS_SIZE);
-				buffer.append(DecompilerContext.getProperty(IFernflowerPreferences.BANNER).toString());
-				classProcessor.writeClass(cl, buffer);
-				return buffer.convertToStringAndAllowDataDiscard();
-			} catch (Throwable t) {
-				DecompilerContext.getLogger().writeMessage("Class " + cl.qualifiedName + " couldn't be fully decompiled.", t);
-				return null;
-			}
-		}
-
-		protected ClassesProcessor classProcessor;
-		protected IdentifierConverter converter;
-	}
-
-	private static class BytecodeProvider implements IBytecodeProvider {
-		BytecodeProvider(ClassEnv env, NameType nameType) {
-			this.env = env;
-			this.nameType = nameType;
-		}
+		public void acceptDirectory(String directory) { }
 
 		@Override
-		public byte[] getBytecode(String externalPath, String internalPath) throws IOException {
-			if (DEBUG) System.out.printf("getBytecode(%s, %s)%n", externalPath, internalPath);
+		public void acceptOther(String path) { }
 
-			if (externalPath.startsWith(pathPrefix) && externalPath.endsWith(pathSuffix)) {
-				String name = externalPath.substring(pathPrefix.length(), externalPath.length() - pathSuffix.length());
-				byte[] ret = get(name);
-				if (ret != null) return ret;
-			}
-
-			throw new FileNotFoundException("can't find class for "+externalPath);
-		}
-
-		public byte[] get(String name) {
-			byte[] ret = cache.get(name);
-			if (ret != null) return ret;
-
-			ClassInstance cls = env.getClsByName(name, nameType);
-
-			if (cls != null) {
-				ret = cls.serialize(nameType);
-				cache.put(name, ret);
-			}
-
-			return ret;
-		}
-
-		protected final ClassEnv env;
-		protected final NameType nameType;
-
-		private final Map<String, byte[]> cache = new HashMap<>();
+		@Override
+		public void close() throws IOException { }
 	}
 
 	private static final boolean DEBUG = false;
 
-	private static final String pathPrefix = "/matchenv/";
-	private static final String pathSuffix = ".class";
 	private static final String unownedUnitFilename = "foreign";
 }
